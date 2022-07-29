a490d21177eafbce6b97462b3c340f6c126d8679
lourvens <lourvens.luxamar@gmail.com>
2022-07-29 23:51:54 +0200

    null

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'],
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     colors: {
       // colors for light theme
